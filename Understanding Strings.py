0d0c1e8d61a8c29dd690a060e07721931d2374fd
Rahul Bhardwaj <98464340+FreaPK@users.noreply.github.com>
2026-01-01 15:30:18 +0530

    Add files via upload

---
+++ Understanding Strings.py
@@ -0,0 +1,19 @@
+#'str' stands for strings
+#'',"",This is used for long strings''' ''', can be used to write strings
+#can use special characters, alphabets, and numbers etc...
+
+x='''Type
+1
+2
+3'''
+print(x)
+print(type(x))
+
+#Concatenation in strings
+first_name='Rahul'
+second_name='Bhardwaj'
+full_name=first_name+' '+second_name
+print(full_name)
+
+
+
